6d1d2a5367350f527fdef2fabcf9c60adc28d75b
Victor Tan <victortan102@gmail.com>
2025-06-19 13:35:08 +0800

    Update css for summary table in user.html

--- static/js/user_analysis.js
+++ static/js/user_analysis.js
@@ -108,7 +108,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
                     // Table
                     const table = document.createElement('table');
-                    table.className = 'table table-bordered table-striped mt-5';
+                    table.className = 'table table-bordered table-striped mt-5 results-table';
                     const keys = ["Game_Start", "Game_End", "Overall_Results", "Score", "Status"];
 
 
@@ -150,8 +150,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
                         rowHtml += `
                         <td>
-                            <button class="btn btn-sm btn-outline-secondary analyze-btn" data-index='${rowIndex}' title="Analyze this row">
-                                🔍
+                            <button class="btn analyze-btn" data-index='${rowIndex}' title="Analyze this row">
+                                <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" class="sparkle">
+                                    <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
+                                </svg>
+
+                                <span class="text">Generate</span>
                             </button>
                         </td>`;
 
